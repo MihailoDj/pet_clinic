d42fc3035bb04191a531aea6419db77c46bbfb08
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2021-11-04 01:13:38 +0100

    Creating service interfaces for base entity classes.

---
+++ pet_clinic_data/src/main/java/com/mihailo/pet_clinic/services/OwnerService.java
@@ -0,0 +1,12 @@
+package com.mihailo.pet_clinic.services;
+
+import com.mihailo.pet_clinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+    Owner findByLastName(String lastName);
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+}
---
+++ pet_clinic_data/src/main/java/com/mihailo/pet_clinic/services/VetService.java
@@ -0,0 +1,11 @@
+package com.mihailo.pet_clinic.services;
+
+import com.mihailo.pet_clinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
