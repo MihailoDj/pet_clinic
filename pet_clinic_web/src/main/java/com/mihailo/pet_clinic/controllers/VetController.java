4b76122361d0552bd7b88b2af45458461580eee6
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2021-11-04 21:50:48 +0100

    Adding pages and controllers for vets and owners.

---
+++ pet_clinic_web/src/main/java/com/mihailo/pet_clinic/controllers/VetController.java
@@ -0,0 +1,15 @@
+package com.mihailo.pet_clinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/vets")
+@Controller
+public class VetController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
+
+}
