2ce7c0649c522366ed74445a175c706b368473e1
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2021-11-10 23:29:49 +0100

    Refactoring map impl-s to have auto ID generation.

--- pet_clinic_web/src/main/java/com/mihailo/pet_clinic/bootstrap/DataLoader.java
+++ pet_clinic_web/src/main/java/com/mihailo/pet_clinic/bootstrap/DataLoader.java
@@ -20,28 +20,24 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 =  new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Mihailo");
         owner1.setLastName("Djordjevic");
 
         ownerService.save(owner1);
 
         Owner owner2 =  new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Pera");
         owner2.setLastName("Peric");
 
         ownerService.save(owner2);
 
         Vet vet1 =  new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Zika");
         vet1.setLastName("Zikic");
 
         vetService.save(vet1);
 
         Vet vet2 =  new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Mika");
         vet2.setLastName("Mikic");
 
