0e6b0934d2e989392b2bc604434e2d659b654271
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2021-11-20 22:44:23 +0100

    Adding pet types to the in-memory data loader class.

--- pet_clinic_web/src/main/java/com/mihailo/pet_clinic/bootstrap/DataLoader.java
+++ pet_clinic_web/src/main/java/com/mihailo/pet_clinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.mihailo.pet_clinic.bootstrap;
 
 import com.mihailo.pet_clinic.model.Owner;
+import com.mihailo.pet_clinic.model.PetType;
 import com.mihailo.pet_clinic.model.Vet;
 import com.mihailo.pet_clinic.services.OwnerService;
+import com.mihailo.pet_clinic.services.PetTypeService;
 import com.mihailo.pet_clinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,14 +13,24 @@ import org.springframework.stereotype.Component;
 public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 =  new Owner();
         owner1.setFirstName("Mihailo");
         owner1.setLastName("Djordjevic");
