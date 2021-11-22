e231b69a7dff7c635d938930beea2fc3ce97c0cc
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2021-11-22 21:22:10 +0100

    Adding some in-memory specialities.

--- pet_clinic_data/src/main/java/com/mihailo/pet_clinic/services/map/VetServiceMap.java
+++ pet_clinic_data/src/main/java/com/mihailo/pet_clinic/services/map/VetServiceMap.java
@@ -8,6 +8,12 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+    private final SpecialityMapService specialityMapService;
+
+    public VetServiceMap(SpecialityMapService specialityMapService) {
+        this.specialityMapService = specialityMapService;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -25,6 +31,13 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+        if (object.getSpecialities().size() > 0) {
+            object.getSpecialities().forEach(speciality -> {
+                if (speciality.getId() == null) {
+                    speciality.setId(specialityMapService.save(speciality).getId());
+                }
+            });
+        }
         return super.save(object);
     }
 
--- pet_clinic_web/src/main/java/com/mihailo/pet_clinic/bootstrap/DataLoader.java
+++ pet_clinic_web/src/main/java/com/mihailo/pet_clinic/bootstrap/DataLoader.java
@@ -1,11 +1,9 @@
 package com.mihailo.pet_clinic.bootstrap;
 
-import com.mihailo.pet_clinic.model.Owner;
-import com.mihailo.pet_clinic.model.Pet;
-import com.mihailo.pet_clinic.model.PetType;
-import com.mihailo.pet_clinic.model.Vet;
+import com.mihailo.pet_clinic.model.*;
 import com.mihailo.pet_clinic.services.OwnerService;
 import com.mihailo.pet_clinic.services.PetTypeService;
+import com.mihailo.pet_clinic.services.SpecialityService;
 import com.mihailo.pet_clinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -17,15 +15,25 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
+    private final SpecialityService specialityService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
+        this.specialityService = specialityService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        int countPetTypes = petTypeService.findAll().size();
+
+        if (countPetTypes == 0){
+            loadData();
+        }
+    }
+
+    private void loadData() {
         PetType dog = new PetType();
         dog.setName("Dog");
         PetType savedDogPetType = petTypeService.save(dog);
@@ -68,15 +76,29 @@ public class DataLoader implements CommandLineRunner {
 
         ownerService.save(owner2);
 
+        Speciality radiology = new Speciality();
+        radiology.setDescription("Radiology");
+        Speciality savedRadiologySpeciality = specialityService.save(radiology);
+
+        Speciality surgery = new Speciality();
+        surgery.setDescription("Surgery");
+        Speciality savedSurgerySpeciality = specialityService.save(surgery);
+
+        Speciality dentistry = new Speciality();
+        dentistry.setDescription("Dentistry");
+        Speciality savedDentistrySpeciality = specialityService.save(dentistry);
+
         Vet vet1 =  new Vet();
         vet1.setFirstName("Zika");
         vet1.setLastName("Zikic");
+        vet1.getSpecialities().add(savedRadiologySpeciality);
 
         vetService.save(vet1);
 
         Vet vet2 =  new Vet();
         vet2.setFirstName("Mika");
         vet2.setLastName("Mikic");
+        vet2.getSpecialities().add(savedDentistrySpeciality);
 
         vetService.save(vet2);
     }
