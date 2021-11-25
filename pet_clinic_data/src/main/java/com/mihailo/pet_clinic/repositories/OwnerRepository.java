2b4575f8a7fd0868dea8662e8547b5c8df3cf317
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2021-11-25 20:46:16 +0100

    Adding Spring Data JPA service for Owner class.

--- pet_clinic_data/src/main/java/com/mihailo/pet_clinic/repositories/OwnerRepository.java
+++ pet_clinic_data/src/main/java/com/mihailo/pet_clinic/repositories/OwnerRepository.java
@@ -4,4 +4,5 @@ import com.mihailo.pet_clinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
