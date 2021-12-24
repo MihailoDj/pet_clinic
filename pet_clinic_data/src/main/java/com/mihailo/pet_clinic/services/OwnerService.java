addbae5b39af524d466fecc6ca207b7960c244e2
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2021-12-24 18:08:20 +0100

    Implementing Owner search functionality.

--- pet_clinic_data/src/main/java/com/mihailo/pet_clinic/services/OwnerService.java
+++ pet_clinic_data/src/main/java/com/mihailo/pet_clinic/services/OwnerService.java
@@ -2,6 +2,9 @@ package com.mihailo.pet_clinic.services;
 
 import com.mihailo.pet_clinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long>{
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
